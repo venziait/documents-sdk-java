c61bcca3f3c15b293069c8a1a3616040a6dcee78
MacMini <MacMini@macmini.local>
2016-03-22 13:46:34 +0100

    ci update sdk

--- src/main/java/com/viafirma/documents/sdk/java/api/V3messagesApi.java
+++ src/main/java/com/viafirma/documents/sdk/java/api/V3messagesApi.java
@@ -77,6 +77,61 @@ public class V3messagesApi {
   }
   
     
+  public Message rejectMessageByCode (String messageCode, String comment) throws ApiException {
+    Object postBody = null;
+    
+
+    // create path and map variables
+    String path = "/v3/messages/reject/{messageCode}".replaceAll("\\{format\\}","json")
+      .replaceAll("\\{" + "messageCode" + "\\}", ApiInvoker.getInstance().escapeString(messageCode.toString()));
+
+    // query params
+    Map<String, String> queryParams = new HashMap<String, String>();
+    Map<String, String> headerParams = new HashMap<String, String>();
+    Map<String, String> formParams = new HashMap<String, String>();
+
+    
+    
+    String[] contentTypes = {
+      "application/x-www-form-urlencoded"
+    };
+
+    String contentType = contentTypes.length > 0 ? contentTypes[0] : "application/json";
+
+    if(contentType.startsWith("multipart/form-data")) {
+      boolean hasFields = false;
+      FormDataMultiPart mp = new FormDataMultiPart();
+      
+      hasFields = true;
+      mp.field("comment", comment, MediaType.MULTIPART_FORM_DATA_TYPE);
+      
+      if(hasFields)
+        postBody = mp;
+    }
+    else {
+      formParams.put("comment", comment);
+      
+    }
+
+    try {
+      String response = ApiInvoker.getInstance().invokeAPI(path, "PUT", queryParams, postBody, headerParams, formParams, contentType);
+      if(response != null){
+        return (Message) ApiInvoker.deserialize(response, "", Message.class);
+      }
+      else {
+        return null;
+      }
+    } catch (ApiException ex) {
+      if(ex.getCode() == 404) {
+      	return  null;
+      }
+      else {
+        throw ex;
+      }
+    }
+  }
+  
+    
   public Message getMessageByCode (String messageCode) throws ApiException {
     Object postBody = null;
     
